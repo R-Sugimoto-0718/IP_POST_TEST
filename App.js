0485ba9ec98e065887ffe6fc96444c498e08d57f
waspryo <sugimoto@liddell.tokyo>
2020-06-24 00:40:22 +0900

    GETとPOST

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null
+    }
+  }
+
+  componentDidMount() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((res) => res.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+
+    // POSTの仕方
+
+    // fetch('https://facebook.github.io/react-native/movies.json', {
+    //   method: 'POPST',
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     firstParams: 'yourValue',
+    //     secondParam: 'yourOtherValue'
+    //   })
+    // })
+  }
+
+  render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      )
+    } else {
+
+      let movies = this.state.dataSource.map((val, key) => {
+        return <View key={key} style={styles.item}>
+          <Text>{val.title}</Text>
+        </View>
+      })
+
+      return (
+        <View style={styles.container}>
+          {movies}
+        </View>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +71,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+    flex: 1,
+    alignSelf: 'stretch',
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee'
+  }
 });
